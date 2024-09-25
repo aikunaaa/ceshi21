ac46e072b272d12995c2ec8466b27be5408f069b
aikunaaa <143726710+aikunaaa@users.noreply.github.com>
2024-09-25 19:03:00 +0800

    Update index.php

--- ez_md5/src/index.php
+++ ez_md5/src/index.php
@@ -1,27 +1,24 @@
 <?php
-include "flag.php";
-$md5_1=$_GET['jiangsir1'];
-$md5_2=$_POST['jiangsir2'];
-$md5_3=$_POST['jiangsir3'];
-$md5_4=$_POST['jiangsir4'];
-$md5_5=$_POST['jiangsir5'];
-if($md5_1 == 0&&!is_numeric($md5_1)&&!is_numeric($md5_2)&&md5($md5_2) == 0e1615313413546846894351431658416514351635153513216354168549846453134684354135212)
-{
-    if($md5_3==md5($md5_3)){
-        if(md5($md5_4)==md5($md5_5) & $md5_4 != $md5_5)
-        {
-        echo $flag;
+header("Content-type:text/html;charset=utf-8");
+error_reporting(0);
+highlight_string(file_get_contents(__FILE__));
+$md5_1 = $_GET['jiangsir1'];
+$md5_2 = $_POST['jiangsir2'];
+$md5_3 = $_POST['jiangsir3'];
+$md5_4 = $_POST['jiangsir4'];
+$md5_5 = $_POST['jiangsir5'];
+
+if ($md5_1 == 0 && !is_numeric($md5_1) && !is_numeric($md5_2) && md5($md5_2) == '0e1615313413546846894351431658416514351635153513216354168549846453134684354135212') {
+    if ($md5_3 == md5($md5_3)) {
+        if (md5($md5_4) == md5($md5_5) && $md5_4 != $md5_5) {
+            echo $flag;
+        } else {
+            echo "jiangsirä¼šå‘Šè¯‰ä½ å¿«äº†å—äº†";
         }
-        else{
-            echo "jiangsir»á¸æËßÄã¿ìÁË¿éÁË";
-        } 
+    } else {
+        echo "jiangsirä¸€å®šä¼šå‘Šè¯‰ä½ ä¸xingçš„";
     }
-    else{
-        echo "jiangsirÒ»¶¨»á¸æËßÄã²»xingµÄ";
-    }
-}
-else
-{
-    echo "jiangsir»á¸æËßÄãnonononoÃ»Ñ§Ã÷°×";
+} else {
+    echo "jiangsirä¼šå‘Šè¯‰ä½ nonononoæ²¡å­¦æ˜Žç™½";
 }
 ?>
